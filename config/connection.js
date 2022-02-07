76ff88c6c099d553801ee29e3d4aad8ebcc81595
ohdust <parents23MD?>
2022-02-07 16:27:31 -0500

    Added unix time stamp to chat msgs

--- config/connection.js
+++ config/connection.js
@@ -43,7 +43,7 @@ class Database {
 
 const connectDB = (dbName, dbPassword) => {
     const db = new Database({
-      host: "localhost",
+      host: "us-cdbr-east-05.cleardb.net",
       user: "b1af0f7b564f9f",
       password: "af5e45f0",
       database: "heroku_1d0b472b7c786fd"
